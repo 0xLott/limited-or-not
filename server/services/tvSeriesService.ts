8f565220255708ec0ea7e07d06b3f77c4075dd1b
0xLott <llott1710@gmail.com>
2025-02-22 01:41:00 -0300

    refactor: enhance error handling and improve variable naming in TVSeriesService

--- server/services/tvSeriesService.ts
+++ server/services/tvSeriesService.ts
@@ -1,9 +1,8 @@
 import { TVSeries, Status } from "../models/TVSeries";
 import { fetchData } from "../utils/fetchData";
-import { TMDB_API } from "../config";
 import { APIError, ObjectNotFoundError, InvalidParameterError } from "../utils/errors";
+import { TMDB_API } from "../config";
 
-// TODO: Alternative for when there's no poster image
 export class TVSeriesService {
   public async getTVSeriesById(seriesId: number): Promise<TVSeries> {
     if (typeof seriesId !== "number" || isNaN(seriesId) || seriesId < -2147483648 || seriesId > 2147483647) {
@@ -38,17 +37,18 @@ export class TVSeriesService {
 
     try {
       const data = await fetchData(url);
-      return data.results.map((item: any) => TVSeriesService.newTvSeriesEntity(item));
+      return data.results.map((tvSeries: any) => TVSeriesService.newTvSeriesEntity(tvSeries));
     } catch (error: any) {
       if (error instanceof ObjectNotFoundError) {
         console.error(`TV Show with title ${seriesTitle} not found.`, error);
-      }
-      if (error instanceof APIError) {
+        throw error;
+      } else if (error instanceof APIError) {
         console.error(`Error from TMDB API: ${error.message}`, error);
         throw error;
+      } else {
+        console.error(`Unexpected error: ${error.message}`, error);
+        throw error;
       }
-      console.error(`Unexpected error: ${error.message}`, error);
-      throw error;
     }
   }
 
