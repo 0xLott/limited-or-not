0a029c1b609f075e936bcbbf6d2fefd2e0498c8c
0xLott <llott1710@gmail.com>
2025-02-23 05:55:18 -0300

    refactor: reusable error handling

--- server/controllers/tvSeriesController.ts
+++ server/controllers/tvSeriesController.ts
@@ -1,24 +1,53 @@
 import type { BunRequest } from "bun";
 import { TVSeriesService } from "../services/tvSeriesService";
+import { APIError, InvalidParameterError, ObjectNotFoundError } from "../utils/errors";
 
 const tvSeriesService = new TVSeriesService();
 
 export const getTVSeriesById = async (req: BunRequest) => {
-  const seriesId = parseInt(req.params.id);
-
-  const series = await tvSeriesService.getTVSeriesById(seriesId);
-
-  return new Response(JSON.stringify(series), {
-    headers: { "Content-Type": "application/json" },
-  });
+  try {
+    const seriesId = parseInt(req.params.id);
+    const series = await tvSeriesService.getTVSeriesById(seriesId);
+    return new Response(JSON.stringify(series), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return handleErrorResponse(error);
+  }
 };
 
 export const searchTVSeriesByTitle = async (req: BunRequest) => {
-  const seriesTitle = req.params.title;
+  try {
+    const title = req.params.title;
+    const series = await tvSeriesService.searchTVSeriesByTitle(title);
+    return new Response(JSON.stringify(series), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return handleErrorResponse(error);
+  }
+};
 
-  const searchResult = await tvSeriesService.searchTVSeriesByTitle(seriesTitle);
+const handleErrorResponse = (error: unknown) => {
+  let status;
+  let message;
 
-  return new Response(JSON.stringify(searchResult), {
+  if (error instanceof InvalidParameterError) {
+    status = 400; // Bad Request
+    message = error.message;
+  } else if (error instanceof ObjectNotFoundError) {
+    status = 404; // Not Found
+    message = error.message;
+  } else if (error instanceof APIError) {
+    status = 502; // Bad Gateway
+    message = error.message;
+  } else {
+    status = 500; // Internal Server Error
+    message = "Internal Server Error";
+  }
+
+  return new Response(JSON.stringify({ error: message }), {
+    status,
     headers: { "Content-Type": "application/json" },
   });
 };
