a67b6feace06abc09f5f856b93b8b87c085bedac
0xLott <llott1710@gmail.com>
2025-02-26 01:02:58 -0300

    refactor: tests folder structure

---
+++ server/tests/serviceLayerTests/fetch.test.ts
@@ -0,0 +1,58 @@
+import { beforeAll, describe, expect, test } from "bun:test";
+import { TVSeriesService } from "../../services/tvSeriesService";
+import { TVSeries } from "../../models/TVSeries";
+import { InvalidParameterError, ObjectNotFoundError } from "../../utils/errors";
+
+var tvSeriesService: TVSeriesService;
+
+describe("Fetching tv series unit tests", () => {
+  beforeAll(() => {
+    tvSeriesService = new TVSeriesService();
+  });
+
+  test("Fetch TV series by valid ID", async () => {
+    const series: TVSeries = await tvSeriesService.getTVSeriesById(7225);
+    expect(series).not.toBeNull();
+    expect(series.id).toBe(7225);
+    expect(series.title).toBe("Merlin");
+  });
+
+  test("Fetch TV series by invalid ID", async () => {
+    expect(async () => {
+      await tvSeriesService.getTVSeriesById(-2147483649);
+    }).toThrow(InvalidParameterError);
+
+    try {
+      await tvSeriesService.getTVSeriesById(-2147483649);
+    } catch (error: any) {
+      expect(error).toHaveProperty("statusCode", 400);
+      expect(error.message).toBe("`seriesId` must be a valid int32");
+    }
+  });
+
+  test("Fetch TV series with invalid input type", async () => {
+    expect(async () => {
+      await tvSeriesService.getTVSeriesById("invalidId" as any);
+    }).toThrow(InvalidParameterError);
+
+    try {
+      await tvSeriesService.getTVSeriesById("invalidId" as any);
+    } catch (error: any) {
+      expect(error).toHaveProperty("statusCode", 400);
+      expect(error.message).toBe("`seriesId` must be a valid int32");
+    }
+  });
+
+  test("Fetch non-existent TV series", async () => {
+    expect(async () => {
+      await tvSeriesService.getTVSeriesById(30000000);
+    }).toThrow(ObjectNotFoundError);
+
+    try {
+      await tvSeriesService.getTVSeriesById(30000000);
+    } catch (error: any) {
+      expect(error).toHaveProperty("statusCode", 404);
+      expect(error.message).toBe("TV Show not found. (HTTP 404)");
+    }
+  });
+});
