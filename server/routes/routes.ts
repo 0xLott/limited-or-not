ea7b4c7aabe58651499876d6a4255fbc2dac4546
0xLott <llott1710@gmail.com>
2025-02-27 02:30:22 -0300

    feat: isMiniseries controller

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,9 +1,10 @@
 import type { BunRequest } from "bun";
-import { getTVSeriesById, searchTVSeriesByTitle } from "../controllers/tvSeriesController";
+import { getTVSeriesById, isMiniseries, searchTVSeriesByTitle } from "../controllers/tvSeriesController";
 
 export const routes: Record<string, (req: BunRequest) => Response | Promise<Response>> = {
   "/": () => Response.json({ message: "Limited or Not?" }),
   "/api/status": () => new Response("OK"),
   "/series/:id": (req) => getTVSeriesById(req),
+  "/series/miniseries/:id": (req) => isMiniseries(req),
   "/search/:title": (req) => searchTVSeriesByTitle(req),
 };
