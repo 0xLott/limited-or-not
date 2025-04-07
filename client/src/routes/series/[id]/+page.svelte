7d1cdd762670b2154a8dae0f928f6d1d35080c9a
0xLott <llott1710@gmail.com>
2025-04-07 02:35:55 -0300

    feat: series loading with dynamic data binding

--- client/src/routes/series/[id]/+page.svelte
+++ client/src/routes/series/[id]/+page.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
     import Footer from '$lib/components/Footer.svelte';
+    let { data } = $props();
 </script>
 
 <div>
     <a href="/">Back</a>
 
     <div class="series-info">
-        <img src="https://plus.unsplash.com/premium_photo-1698584583491-5666d00cae91?q=80&w=300" alt="Poster">
-        <h1>Series A</h1>
-        <p>✅ is a miniseries</p>
-    
-        <span>Status: Ended</span>
-        <span>Number of seasons: 1</span>
+        <img src={data.series.poster_path} alt="Poster">
+        <h1>{data.series.title}</h1>
+
+        {#if data.series.isMiniseries}
+            <p>✅ is a miniseries</p>
+        {:else}
+            <p>❌ is not a miniseries</p>
+        {/if}
+        
+        <span>id: {data.series.id}</span>
+        <span>Status: {data.series.status}</span>
+        <span>Number of seasons:</span>
     </div>
 
     <div class="series-overview">
