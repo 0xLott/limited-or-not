0ad3bc117e5ecf8107d967767db4f00b37c3f0f2
0xLott <llott1710@gmail.com>
2025-04-18 01:27:07 -0300

    feat: implement TV series card component

--- client/src/routes/search/[query]/+page.server.ts
+++ client/src/routes/search/[query]/+page.server.ts
@@ -5,12 +5,14 @@ import type TVSeries from '$types/TVSeries';
 export async function load({ params }) {
     const BASE_URL: String = SERVER_URL
 
-    const response = await fetch(`${BASE_URL}/search/${decodeURIComponent(params.query)}`);
-    const searchResults: TVSeries = await response.json();
+    const query = decodeURIComponent(params.query)
+    const response = await fetch(`${BASE_URL}/search/${query})}`);
+    const searchResults: TVSeries[] = await response.json();
 
     if (response.status !== 200) error(response.status);
 
     return {
+        query,
         searchResults
     };
 }
