0fcd70aa83ace4621265e394bd72cec8a45dad70
0xLott <llott1710@gmail.com>
2026-02-08 00:44:39 -0300

    fix: response validation before object mappping

--- client/src/routes/search/[query]/+page.server.ts
+++ client/src/routes/search/[query]/+page.server.ts
@@ -6,10 +6,11 @@ export async function load({ params }) {
     const BASE_URL: String = SERVER_URL
 
     const query = decodeURIComponent(params.query)
-    const response = await fetch(`${BASE_URL}/search/${query})}`);
-    const searchResults: TVSeries[] = await response.json();
+    const response = await fetch(`${BASE_URL}/search/${query}`);
 
     if (response.status !== 200) error(response.status);
+
+    const searchResults: TVSeries[] = await response.json();
 
     return {
         query,
--- client/src/routes/series/[id]/+page.server.ts
+++ client/src/routes/series/[id]/+page.server.ts
@@ -6,9 +6,10 @@ export async function load({ params }) {
     const BASE_URL: String = SERVER_URL
 
     const response = await fetch(`${BASE_URL}/series/${params.id}`);
-    const series: TVSeries = await response.json(); // Parses JSON response into TVSeries object, assuming the data conforms to the interface
 
     if (response.status !== 200) error(response.status);
+
+    const series: TVSeries = await response.json();
 
     return {
         series
